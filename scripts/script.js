6bc3f4668d7a4c767784ed777171370708d20399
Matheus Iago <120859355+theus-iago@users.noreply.github.com>
2023-06-21 05:09:30 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,14 @@
 tela = document.getElementById('tela');
+
 function insert(num) {
 document.getElementById('tela').innerHTML += num;
+}
+function clean() {
+  document.getElementById('tela').innerHTML = "";
+}
+function back() {
+  Document.getElementById('tela').substring= "0, document.getElementById('tela').innerHTML= -1");
+}
+function result() {
+  document.getElementById('tela').innerHTML= eval();
 }
